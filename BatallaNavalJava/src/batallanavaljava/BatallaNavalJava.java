77ab88887035ac2eabb809e3254e22630fc745ed
Maxi Montenegro <maxi.montenegro.umi@gmail.com>
2024-10-06 11:01:42 -0300

    Seteando el ambiente. Repo en github listo y creando archivo inicial en Java. listos para la diversion

---
+++ BatallaNavalJava/src/batallanavaljava/BatallaNavalJava.java
@@ -0,0 +1,11 @@
+package batallanavaljava;
+
+
+public class BatallaNavalJava {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola GoldenBytes!");
+    }
+    
+}
