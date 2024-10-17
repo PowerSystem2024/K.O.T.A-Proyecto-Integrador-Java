df358b2d5ad483ee9dd1d2f8886a06732dacca3c
Franco Poblete <168313957+carfrank84@users.noreply.github.com>
2024-10-17 16:44:27 -0300

    termiando la conversion del codigo a java, ataque del enemigo

--- BatallaNavalJava/src/batallanavaljava/AtaqueDelEnemigo.java
+++ BatallaNavalJava/src/batallanavaljava/AtaqueDelEnemigo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package batallanavaljava;
 
-/**
- *
- * @author Mkjdf983
- */
 public class AtaqueDelEnemigo {
         public static void ataqueDelEnemigo(int[][] matrizJugador, int[] contadorBarcosJugador) {
         int fila, columna;
@@ -19,18 +12,18 @@ public class AtaqueDelEnemigo {
 
         // Verificar coordenadas
         switch (matrizJugador[fila][columna]) {
-            case 0:
+            case 0 -> {
                 System.out.println("El enemigo le ha dado al agua.");
                 matrizJugador[fila][columna] = -1; // Representar agua
-                break;
-            case 6:
+                }
+            case 6 -> {
                 System.out.println("El enemigo ha dañado tu lancha.");
                 matrizJugador[fila][columna] = -6; // Lancha dañada
                 contadorBarcosJugador[4]--;
-                break;
-            // Agregar casos para otros barcos
-            default:
-                break;
+                }
+            default -> {
+                }
         }
-    }
+            // Agregar casos para otros barcos
+                }
 }
