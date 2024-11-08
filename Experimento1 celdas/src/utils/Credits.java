b83a97310844865a27aa37e85f68b3aa221b6162
Franco Poblete <168313957+carfrank84@users.noreply.github.com>
2024-11-08 19:10:52 -0300

    crando la animacion , mensaje de bienvenida, creditos y reglas del juego

--- Experimento1 celdas/src/utils/Credits.java
+++ Experimento1 celdas/src/utils/Credits.java
@@ -1,10 +1,17 @@
-
 package utils;
 
-/**
- *
- * @author Mkjdf983
- */
 public class Credits {
-    
+
+    public void mostrarCreditos() {
+        System.out.println("\n----- Batalla Naval -----");
+        System.out.println("This game was developed by the team:");
+        System.out.println("Knights of the Algoritmo (K.O.T.A)");
+        System.out.println("Developers:");
+        System.out.println("Maximiliano Montenegro");
+        System.out.println("Franco Poblete");
+        System.out.println("\nProject: Integrative Project");
+        System.out.println("Technicature in Programming - Cohort 2024");
+        System.out.println("UTN FRSR");
+        System.out.println("\nPress Enter to return to the main menu.");
+    }
 }
--- Experimento1 celdas/src/utils/Menu.java
+++ Experimento1 celdas/src/utils/Menu.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils;
 
-/**
- *
- * @author Mkjdf983
- */
+import java.util.Scanner;
+import test.TestBatallaNaval;
+
 public class Menu {
-    
+
+    public void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("----- Main Menu -----");
+            System.out.println("1. Play");
+            System.out.println("2. Game Rules");
+            System.out.println("3. Credits");
+            System.out.println("4. Exit");
+            System.out.print("Select an option: ");
+
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                switch (opcion) {
+                    case 1:
+                        iniciarJuego(); // Llamada al método que inicia el juego
+                        break;
+                    case 2:
+                        showRules();
+                        break;
+                    case 3:
+                        showCredits(); // Mostrar los créditos
+                        break;
+                    case 4:
+                        System.out.println("Exiting the game. See you soon!");
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+            } else {
+                System.out.println("Please, enter a valid number.");
+                scanner.next(); // Limpiar la entrada incorrecta
+            }
+        } while (opcion != 4);
+
+        scanner.close();
+    }
+
+    private void iniciarJuego() {
+        System.out.println("Starting the game...");
+        TestBatallaNaval.main(new String[]{}); // Llama al método main de la clase TestBatallaNaval
+    }
+
+    private void showRules() {
+        // Llamamos a la clase GameRules para mostrar las reglas
+        GameRules gameRules = new GameRules();
+        gameRules.showRules();
+    }
+
+    private void showCredits() {
+        // Llamamos a la clase Credits para mostrar los créditos
+        Credits credits = new Credits();
+        credits.mostrarCreditos();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine(); // Esperar a que presionen Enter
+    }
 }
